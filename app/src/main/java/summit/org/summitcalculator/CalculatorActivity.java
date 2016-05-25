691e9150139fc5353d7ba17b8e76d820a46404c6
Naveed <Naveed.Khan2@capitalone.com>
2016-05-24 20:15:49 -0400

    Added calculator UI

--- app/src/main/java/summit/org/summitcalculator/CalculatorActivity.java
+++ app/src/main/java/summit/org/summitcalculator/CalculatorActivity.java
@@ -15,5 +15,7 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/summit/org/summitcalculator/TipCalculatorActivity.java
+++ app/src/main/java/summit/org/summitcalculator/TipCalculatorActivity.java
@@ -2,6 +2,7 @@ package summit.org.summitcalculator;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,5 +16,12 @@ public class TipCalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calculator);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //TODO Set the actionbar title for this activity
+        //TODO Set listeners on buttons
+        //TODO Implement tip calculation logic
+        //TODO Show the result to the user using the input from edit texts
+
     }
 }
