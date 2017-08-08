1386aaf586f270f91f7cc5086a1507e8351ae7b6
zkovar <zachary.kovar@capitalone.com>
2017-08-08 16:40:57 -0400

    Updating gradle build files

--- app/src/main/java/summit/org/summitcalculator/TipCalculatorActivity.java
+++ app/src/main/java/summit/org/summitcalculator/TipCalculatorActivity.java
@@ -2,7 +2,6 @@ package summit.org.summitcalculator;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
