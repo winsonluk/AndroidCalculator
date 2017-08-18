d6730c459ea39d712f8173453692297a82df9d74
Winson <winson.luk@gmail.com>
2017-08-18 07:19:10 -0700

    update

--- app/src/main/java/summit/org/summitcalculator/CalculatorActivity.java
+++ app/src/main/java/summit/org/summitcalculator/CalculatorActivity.java
@@ -4,8 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String string0 = "";
+    private int input0;
+    private String operator;
+    private String string1 = "";
+    private int input1;
 
     public static Intent getStartIntent(Context context) {
         return new Intent(context, CalculatorActivity.class);
@@ -18,8 +28,163 @@ public class CalculatorActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //TODO set correct title for action bar
+        Button b0 = (Button)findViewById(R.id.b0);
+        Button b1 = (Button)findViewById(R.id.b1);
+        Button b2 = (Button)findViewById(R.id.b2);
+        Button b3 = (Button)findViewById(R.id.b3);
+        Button b4 = (Button)findViewById(R.id.b4);
+        Button b5 = (Button)findViewById(R.id.b5);
+        Button b6 = (Button)findViewById(R.id.b6);
+        Button b7 = (Button)findViewById(R.id.b7);
+        Button b8 = (Button)findViewById(R.id.b8);
+        Button b9 = (Button)findViewById(R.id.b9);
+        Button bp = (Button)findViewById(R.id.bp);
+        Button bm = (Button)findViewById(R.id.bm);
+        Button bx = (Button)findViewById(R.id.bx);
+        Button bd = (Button)findViewById(R.id.bd);
+        Button be = (Button)findViewById(R.id.be);
+
+        b0.setOnClickListener(this);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+        b8.setOnClickListener(this);
+        b9.setOnClickListener(this);
+        bp.setOnClickListener(this);
+        bm.setOnClickListener(this);
+        bx.setOnClickListener(this);
+        bd.setOnClickListener(this);
+        be.setOnClickListener(this);
+
         //todo grab and set listeners
         //todo Implement calculator 
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.b0:
+            case R.id.b1:
+            case R.id.b2:
+            case R.id.b3:
+            case R.id.b4:
+            case R.id.b5:
+            case R.id.b6:
+            case R.id.b7:
+            case R.id.b8:
+            case R.id.b9:
+                if (operator == null) {
+                    string0 += ((Button) v).getText().toString();
+                    ((TextView) findViewById(R.id.result)).setText(string0);
+                } else {
+                    string1 += ((Button) v).getText().toString();
+                    ((TextView) findViewById(R.id.result)).setText(string1);
+                }
+                break;
+            case R.id.bp:
+                operator = "+";
+                ((TextView) findViewById(R.id.result)).setText("");
+                break;
+            case R.id.bm:
+                operator = "-";
+                ((TextView) findViewById(R.id.result)).setText("");
+                break;
+            case R.id.bx:
+                operator = "*";
+                ((TextView) findViewById(R.id.result)).setText("");
+                break;
+            case R.id.be:
+                //((TextView) findViewById(R.id.result)).setText(string0+operator+string1);
+                String toEval = string0 + " " + operator + " " + string1;
+                string0 = "";
+                string1 = "";
+                operator = "";
+                ((TextView) findViewById(R.id.result)).setText(Double.toString(eval(toEval)));
+        }
+    }
+
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length())
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (; ; ) {
+                    if (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (; ; ) {
+                    if (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
+    }
+
 }
--- app/src/main/java/summit/org/summitcalculator/TipCalculatorActivity.java
+++ app/src/main/java/summit/org/summitcalculator/TipCalculatorActivity.java
@@ -4,8 +4,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class TipCalculatorActivity extends AppCompatActivity {
+import java.text.NumberFormat;
+
+public class TipCalculatorActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private double amount;
+    private double tipPercent;
+
+    private String tipString;
+    private String totalString;
+
+    private EditText totalEditText;
+    private EditText tipEditText;
 
     public static Intent getStartIntent(Context context) {
         return new Intent(context, TipCalculatorActivity.class);
@@ -17,10 +33,24 @@ public class TipCalculatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tip_calculator);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //TODO Set the actionbar title for this activity
-        //TODO Set listeners on buttons
-        //TODO Implement tip calculation logic
-        //TODO Show the result to the user using the input from edit texts
+
+        Button button = (Button)findViewById(R.id.btn_calculate);
+        button.setOnClickListener(this);
+
+        totalEditText = (EditText)findViewById(R.id.et_bill_amount);
+        tipEditText = (EditText)findViewById(R.id.et_tip);
+    }
+
+    public void onClick(View v) {
+        amount = Double.valueOf(totalEditText.getText().toString());
+        tipPercent = Double.valueOf(tipEditText.getText().toString()) / 100;
+
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        tipString = formatter.format(amount * tipPercent);
+        totalString = formatter.format(amount * tipPercent + amount);
+
+        ((TextView)findViewById(R.id.tv_result_tip)).setText(tipString);
+        ((TextView)findViewById(R.id.tv_result_total)).setText(totalString);
 
     }
 }
